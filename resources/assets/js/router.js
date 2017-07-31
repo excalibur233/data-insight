85cf825e25827906f46b399027e5033ae1f9888f
蔡乾 <cai.q@foxmail.com>
2017-07-31 15:57:45 +0800

    Front scaffold fix.

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,14 +1,14 @@
 import Router from 'vue-router';
 
-import Categories from './layouts/Categories.vue';
-import ShopIndex from './layouts/ShopIndex.vue';
-import Profile from './layouts/Profile.vue';
+// import Categories from './layouts/Categories.vue';
+// import ShopIndex from './layouts/ShopIndex.vue';
+// import Profile from './layouts/Profile.vue';
 
 
 const routes = [
-  { path: '/', component: ShopIndex },
-  { path: '/categories', component: Categories },
-  { path: '/profile', component: Profile },
+  // { path: '/', component: ShopIndex },
+  // { path: '/categories', component: Categories },
+  // { path: '/profile', component: Profile },
 ];
 
 export default new Router({
