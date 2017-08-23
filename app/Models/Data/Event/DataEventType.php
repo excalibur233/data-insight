53158bf2d8fb65fd5ed97d6b21b53b7efe6b6095
Damon <whliujun@hotmail.com>
2017-08-23 15:15:28 +0800

    update  models

--- app/Models/Data/Event/DataEventType.php
+++ app/Models/Data/Event/DataEventType.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Data\Event;
 
+use App\Models\Data\Attribute\Traits\BelongsToManyAttributes;
 use App\Models\Data\Event\Traits\HasManyDataEvents;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,5 @@ use Illuminate\Database\Eloquent\Model;
  */
 class DataEventType extends Model
 {
-    use HasManyDataEvents;
+    use HasManyDataEvents, BelongsToManyAttributes;
 }
